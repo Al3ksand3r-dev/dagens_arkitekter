3c0fed8e3ceee53df5daa77bf5f1d03455e923dd
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2021-03-13 16:48:00 +0100

    added sub menu with sub links

--- client/src/components/ui/AppLinks.vue
+++ client/src/components/ui/AppLinks.vue
@@ -2,7 +2,11 @@
   <ul
     class="navigation-list navigation-list--flex navigation-list--col navigation-list--vCenter"
   >
-    <li v-for="link in links" :key="link.label">
+    <li
+      v-for="link in links"
+      :key="link.label"
+      :class="{ 'navigation-list__sub-link': link.label === 'Mer' }"
+    >
       <a
         href="#"
         v-scroll-to="link.hash"
@@ -16,8 +20,22 @@
         class="navigation-list__link"
         :to="{ name: link.url }"
         @click.native="$store.state.isOpen && $store.commit('TOGGLE_SIDEBAR')"
-        >{{ link.label }}</router-link
+        >{{ link.label }}
+      </router-link>
+      <ul
+        v-if="link.subLinks"
+        class="navigation-list navigation-list__sub-menu"
       >
+        <li v-for="subLink in link.subLinks" :key="subLink.elName">
+          <a
+            href="#"
+            class="navigation-list__link"
+            v-scroll-to="subLink.hash"
+            @click="$store.state.isOpen && $store.commit('TOGGLE_SIDEBAR')"
+            >{{ subLink.elName }}</a
+          >
+        </li>
+      </ul>
     </li>
   </ul>
 </template>
@@ -37,16 +55,21 @@ export default {
         hash: "#services",
       },
       {
-        elName: "Försäljningsmaterial",
-        hash: "#material",
-      },
-      {
-        url: "Permits",
-        label: "Brf hemsida",
-      },
-      {
-        elName: "Projektering & Rådgivning",
-        hash: "#project",
+        label: "Mer",
+        subLinks: [
+          {
+            elName: "Försäljningsmaterial",
+            hash: "#material",
+          },
+          {
+            elName: "Brf hemsida",
+            hash: "#brf",
+          },
+          {
+            elName: "Projektering & Rådgivning",
+            hash: "#project",
+          },
+        ],
       },
       {
         url: "Contact",
